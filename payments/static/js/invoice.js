0f3b98e9af60d668974a5836e2da611a989d932a
AJ-54 <ayushjai@iitg.ac.in>
2020-12-11 19:36:16 +0530

    Final Version

--- payments/static/js/invoice.js
+++ payments/static/js/invoice.js
@@ -1,14 +1,9 @@
-<script>
-      var socket = new WebSocket("wss://www.blockonomics.co/payment/"+ "{{addr}}");
-      socket.onmessage = function(event){
-        response = JSON.parse(event.data);
-        console.log(response);
-        //This condition ensures that we reload only when we get a 
-        //new payment status and don't go into a loop
-          if (response.status > '{{invoice_status}}')
-          setTimeout(function(){window.location.reload() }, 1000); 
-        
-      }
-      
-      {/* The current location (invoice page) is reload only upon websocket //notification of payment.There is timeout to let the invoice status //update on server via HTTP callback */}
-</script>
+var socket = new WebSocket("wss://www.blockonomics.co/payment/"+ address);
+socket.onmessage = function(event){
+  response = JSON.parse(event.data);
+  //This condition ensures that we reload only when we get a 
+  //new payment status and don't go into a loop
+    if (parseInt(response.status) > parseInt(status))
+    setTimeout(function(){window.location.reload() }, 1000); 
+  
+}
